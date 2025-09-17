04ef96753da57ee524aa81ffb149276b9ac40f41
DESKTOP-V5661T0\shenhao <937176211@qq.com>
2025-09-17 15:51:31 +0800

    fix: 先禁用日志过滤器

--- server/main.cpp
+++ server/main.cpp
@@ -282,8 +282,8 @@ int start_main(int argc,char *argv[]) {
 #endif//!defined(_WIN32)
 
         //启动异步日志线程
-        Logger::Instance().setWriter(std::make_shared<AsyncLogWriterFilter>(std::make_shared<AsyncLogWriter>()));
-
+        //Logger::Instance().setWriter(std::make_shared<AsyncLogWriterFilter>(std::make_shared<AsyncLogWriter>()));
+        Logger::Instance().setWriter(std::make_shared<AsyncLogWriter>());
         InfoL << kServerName;
 
         //加载配置文件，如果配置文件不存在就创建一个
